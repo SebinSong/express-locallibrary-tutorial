f8d4b309b2d1411350eb2dd473b48efc3994bdd5
SebinSong <songxevin@gmail.com>
2023-06-03 21:03:06 +1200

    DB_URI fallback string

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 require('dotenv').config()
 
+const dbURI = process.env.DB_CONNECTION_STRING || 'mongodb+srv://sebin123:sebin123@cluster0.7at80te.mongodb.net/local_library?retryWrites=true&w=majority'
+
 exports.connectToDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_STRING)
+  await mongoose.connect(dbURI)
 }
